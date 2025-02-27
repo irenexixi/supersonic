a56c1814d8eed896d8eafe39fa9eca60de0a6c0a
liqianqian <liqianqian@migu.cn>
2025-02-27 18:22:56 +0800

    增加h5模式，设置h5模式样式

--- webapp/packages/supersonic-fe/src/pages/index.tsx
+++ webapp/packages/supersonic-fe/src/pages/index.tsx
@@ -4,6 +4,11 @@ import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import { ELEPHANT_USERNAME_KEY } from '@/common/constants';
 
+// 默认设置为h5模式
+const rootELe = document.getElementById('root')
+if (rootELe) {
+  rootELe.classList.add('h5-mode')
+}
 // 直接使用当前页面的 URL
 var url = new URL(window.location.href);
 // 解析查询参数
@@ -11,6 +16,12 @@ const oaAccount = url.searchParams?.get('oaAccount');
 if (oaAccount) {
   localStorage.setItem(ELEPHANT_USERNAME_KEY, oaAccount);
 }
+// 非h5模式去掉class
+const mode = url.searchParams?.get('mode');
+if (!mode && rootELe) {
+  rootELe.classList.remove('h5-mode')
+}
+console.log('mode', mode, '、oaAccount', oaAccount);
 
 NProgress.configure({ showSpinner: false });
 

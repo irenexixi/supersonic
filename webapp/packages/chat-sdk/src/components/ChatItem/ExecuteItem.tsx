c5ee2030d69d36ad10d4a6e6499e332504620d5e
阳光 <shuanqverymuch@gmail.com>
2025-03-14 16:26:09 +0800

    fix: 总结改成智能洞察

--- webapp/packages/chat-sdk/src/components/ChatItem/ExecuteItem.tsx
+++ webapp/packages/chat-sdk/src/components/ChatItem/ExecuteItem.tsx
@@ -169,7 +169,7 @@ const ExecuteItem: React.FC<Props> = ({
           )}
           {data.textSummary && (
             <p className={`${prefixCls}-step-bottom`}>
-              <span className={`${prefixCls}-step-title`} style={{ marginRight: 5 }}>总结:</span>
+              <span className={`${prefixCls}-step-title`} style={{ marginRight: 5 }}>智能洞察:</span>
               {data.textSummary}
             </p>
           )}
